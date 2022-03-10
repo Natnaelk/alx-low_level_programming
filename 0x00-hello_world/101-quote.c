3d6163463aca9ac41af5a9443427d02f763167fd
root <root@localhost.localdomain>
2022-03-10 18:37:45 -0500

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  * Return:1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	fprintf(stderr, "\n");
-	return (1);
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+return (1);
 }
