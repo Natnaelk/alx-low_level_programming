a0b01f0febc0eb44c9209d91f3c775b71d520cfe
root <root@localhost.localdomain>
2022-03-10 17:23:32 -0500

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
+/* this is a new file */
 #include <stdio.h>
 /**
  * this is the main function
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+/**
+ * Return: 0 if there is no error.
+ */
 	return (0);
 }
