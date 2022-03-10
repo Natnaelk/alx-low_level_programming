234e0a4c8d49cf638c2f2bfe426b2e458b0b299a
root <root@localhost.localdomain>
2022-03-10 18:31:09 -0500

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - Entry point
  * Return:1
  */
-int main()
+int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
