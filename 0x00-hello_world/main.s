5b8cf7473f46398a991963fd55dd16ab2b103932
root <root@localhost.localdomain>
2022-03-10 18:16:53 -0500

    new

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,19 +1,20 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
 	.cfi_startproc
-	pushl	%ebp
+	push	ebp
 	.cfi_def_cfa_offset 8
 	.cfi_offset 5, -8
-	movl	%esp, %ebp
+	mov	ebp, esp
 	.cfi_def_cfa_register 5
 	call	__x86.get_pc_thunk.ax
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax
-	movl	$0, %eax
-	popl	%ebp
+	add	eax, OFFSET FLAT:_GLOBAL_OFFSET_TABLE_
+	mov	eax, 0
+	pop	ebp
 	.cfi_restore 5
 	.cfi_def_cfa 4, 4
 	ret
@@ -27,7 +28,7 @@ main:
 __x86.get_pc_thunk.ax:
 .LFB1:
 	.cfi_startproc
-	movl	(%esp), %eax
+	mov	eax, DWORD PTR [esp]
 	ret
 	.cfi_endproc
 .LFE1:
