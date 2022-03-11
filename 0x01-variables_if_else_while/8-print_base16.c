4bea63a5b08c009d544decb988485d5ad6092b53
root <root@NATI.localdomain>
2022-03-11 17:46:16 +0300

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  main - Entery Point
+ *  Return: 0(Success)
+ */
+int main(void)
+{
+	char a, b;
+
+	b='0';
+	while (b <= '9')
+	{
+		putchar(b);
+		b++;
+	}
+	
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
