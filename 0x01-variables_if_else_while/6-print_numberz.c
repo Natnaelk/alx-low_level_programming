58b8f669935a33e937132dc4b48f473ee098d1eb
root <root@NATI.localdomain>
2022-03-11 17:17:55 +0300

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main - Entery Point
+ *  Return: 0(Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(48+a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
