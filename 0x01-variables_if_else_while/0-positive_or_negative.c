fb0cc5e5135a6f593b170b1348d2c836ab4ec82e
root <root@NATI.localdomain>
2022-03-11 14:33:46 +0300

    new

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	printf("is positive\n");
+	printf("%d is positive\n",n);
 	else if (n < 0)
-	printf("is negative\n");
+	printf("%d is negative\n",n);
 	else
-	printf("is zero\n");
+	printf("%d is zero\n",n);
 	return (0);
 }
