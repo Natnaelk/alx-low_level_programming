31e5c8c8c9286eff8087e4477bc5870e58cb4eca
root <root@NATI.localdomain>
2022-03-11 19:39:30 +0300

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i=0; i <= 9 ; i++)
+	for (i = 0; i <= 9 ; i++)
 	{
-		for (j=1; j <= 9 ; j++)
+		for (j = 1; j <= 9 ; j++)
 		{
 			if (i != j)
 			{
-				int k = i*10+i;
-				int l = j*10+i;
+				int k = i * 10 + i;
+				int l = j * 10 + i;
+
 				if (k < l)
 				{
-					putchar(i+48);
-					putchar(j+48);
+					putchar( i + 48);
+					putchar(j + 48);
 					if (k < 80 || l < 80)
 					{
 						putchar(44);
