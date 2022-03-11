0e5182ab2ae98b791e1c1731b2f836878eaa4248
root <root@NATI.localdomain>
2022-03-11 14:55:12 +0300

    new

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	/* your code goes there */
+	if (lastDigit > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	else if (lastDigit < 6 && n != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	else
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	return (0);
+}
