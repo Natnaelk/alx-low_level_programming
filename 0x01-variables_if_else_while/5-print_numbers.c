86a2300d5f19390e1200aa7ca4be58ec340cd122
root <root@NATI.localdomain>
2022-03-11 16:45:09 +0300

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  main - Entery Point
+ *  Return: 0(Success)
+ */
+int main(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
