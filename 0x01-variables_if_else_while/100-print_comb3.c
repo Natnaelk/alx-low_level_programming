de53c3533879ddc09547cc25bea1752502cf7126
root <root@NATI.localdomain>
2022-03-11 18:57:27 +0300

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *  main - Entry Point
+ *  Return: 0(success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i=0; i <= 9 ; i++)
+	{
+		for (j=1; j <= 9 ; j++)
+		{
+			if (i != j)
+			{
+				int k = i*10+i;
+				int l = j*10+i;
+				if (k < l)
+				{
+					putchar(i+48);
+					putchar(j+48);
+					putchar(44);
+					putchar(32);
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
