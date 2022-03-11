27a720cd46ce08140c490d285a5b87c6c2d1c2d9
root <root@NATI.localdomain>
2022-03-11 15:11:28 +0300

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
