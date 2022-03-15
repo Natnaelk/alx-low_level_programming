06861d188916279fed512c89ce9a0f6f6d83bee4
root <root@NATI.localdomain>
2022-03-15 15:24:59 +0300

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  main - Entry Point
+ *  Return - 0(Successful)
+ */
+int main(void)
+{
+	char letter[] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(letter[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
