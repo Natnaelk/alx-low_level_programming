58859c00fcea28da2f66de77b72a42c3d1a04e20
root <root@NATI.localdomain>
2022-03-15 20:58:18 +0300

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,10 +9,10 @@ void print_alphabet(void)
 {
 	    int alp = 0;
 
-	    while (alp < 26)
-	    {
-		_putchar(alp+97);
+	while (alp < 26)
+	{
+		_putchar(alp + 97);
 		alp++;
-	    }
-	    _putchar('\n');
+	}
+	_putchar('\n');
 }
