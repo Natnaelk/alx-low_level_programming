d72c8968bde59309830ea0383a810e162823f039
root <root@NATI.localdomain>
2022-03-15 22:05:19 +0300

    new

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry Point
+ * @c - checking alphabet or not
+ * Return: 0 or 1
+ *
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
