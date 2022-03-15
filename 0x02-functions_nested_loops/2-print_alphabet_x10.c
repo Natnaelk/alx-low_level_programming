fd13b202c6bf0baf9cc1b7a9094c41e11c4e666c
root <root@NATI.localdomain>
2022-03-15 21:23:15 +0300

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 - Entry Point
+ *  Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{
+		int alp = 0;
+		
+		while (alp < 26)
+		{
+			_putchar(alp+97);
+			alp++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
