1fafad004b2fef4c6419c0b9d9ebf1cfd8f4cf82
root <root@NATI.localdomain>
2022-03-15 23:21:10 +0300

    n

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,12 @@
 int print_last_digit(int a)
 {
 	int result;
-	a = a >= 0 ? a : a * -1;
-	result = a % 10;
-	_putchar('0' + result);
 
+	if (a < 0)
+		result = -1 * (a % 10);
+	else
+		result = a % 10;
+
+	_putchar(result + '0');
 	return (result);
 }
