135703faba0059a941558f485001cf2817add841
root <root@NATI.localdomain>
2022-03-15 21:44:42 +0300

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Entry Point
+ * Return:0
+ *
+ */
+int _islower(int c)
+{
+	int result;
+
+	if (c > 96 && c < 123)
+	{
+		result = 1;
+		return result;
+	}
+	else
+	return (0);
+}
