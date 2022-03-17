675bade970e513e3a6358bbfea68414ffa24e2f0
root <root@NATI.localdomain>
2022-03-17 05:25:24 +0300

    m

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,22 +1,31 @@
+#include "holberton.h"
 #include <stdio.h>
-
 /**
- * main - Entry Point
- * Return - 0
+ * main - prints first 50 numbers of the fibonacci sequence
+ * Return: 0
  */
 int main(void)
 {
-	int i, n, num1, num2, num3;
-	
+	int i, n;
+	long f1, f2, next;
+
+	f1 = 1;
+	f2 = 2;
 	n = 50;
-	num1 = 0;
-	num2 = 1;
-	for ( i = 2; i <= n; i++)
+
+	for (i = 0; i < n; i++)
 	{
-		num3 = num1 + num2;
-		num1 = num2;
-		num2 = num3;
-		printf("%d, ", num3);
+		if (i == 49)
+		{
+			printf("%lu", f1);
+		} else
+		{
+			printf("%lu, ", f1);
+			next = f1 + f2;
+			f1 = f2;
+			f2 = next;
+		}
 	}
 	printf("\n");
+	return (0);
 }
