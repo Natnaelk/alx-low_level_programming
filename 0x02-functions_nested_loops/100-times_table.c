c7cb7643fd126f7c7fc2b72c6fe108a5904b3b8a
root <root@NATI.localdomain>
2022-03-17 03:33:57 +0300

    new

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,24 +10,44 @@ void print_times_table(int n)
 	int i;
 	int j;
 
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			int result;
-			if (n > 15 || n < 0)
+			
+			result = i * j;
+			if (j == 0)
+				_putchar('0' + result);
+			else if (result < 10)
 			{
-				break;
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
 			}
 			else
 			{
-			result = i * j;
-			_putchar('0' + result);
-			_putchar(',');
-			_putchar(' ');
+				_putchar('0' + result / 100);
+				_putchar('0' + (result - 100) / 10);
+				_putchar('0' + result % 10);
 			}
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
 		}
 		_putchar('\n');
 	}
-	
 }
