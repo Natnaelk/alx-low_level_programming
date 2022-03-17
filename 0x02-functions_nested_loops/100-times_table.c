89c75da88e672c2fb42150318919266ee77b4f9f
root <root@NATI.localdomain>
2022-03-17 04:07:25 +0300

    n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,51 @@
 #include "main.h"
+
 /**
- * print_times_table - Entry Point
+ * print_times_table - prints times table
  * @n : times table to use
  * Return:void
  */
+
 void print_times_table(int n)
 {
-	int i, j, result;
 
-	if (n > 15 || n < 0)
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-		return;
-	}
-	while (i <= n)
-	{
-		for (j = 0; j <= n; j++)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-			result = i * j;
-			if (j == 0)
-				_putchar('0' + result);
-			else if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result);
-			}
-			else if (result < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + result / 10);
-				_putchar('0' + result % 10);
-			}
-			else
-			{
-				_putchar('0' + result / 100);
-				_putchar('0' + (result - 100) / 10);
-				_putchar('0' + result % 10);
-			}
-			if (j < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
 		}
-		i++;
-		_putchar('\n');
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
+	_putchar('\n');
+	a++;
+}
 }
