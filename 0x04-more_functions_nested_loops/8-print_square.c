e97ccb32c4bc5eb95fd0ee332c7d08f4da1cb4d5
root <root@NATI.localdomain>
2022-03-17 23:15:13 +0300

    n

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,29 @@
 #include "main.h"
+
 /**
- * print_square - Entry Point
- * @size : the size of the square
- * Return:square
+ * print_square - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
  */
-void print_square(int size)
+
+void print_square(int n)
 {
-	int i;
-	int j;
-	
-	if (size == 0)
+
+	int i = 0, ii;
+
+	while (i < n && n > 0)
 	{
-		_putchar('\n');
-	}
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
+		ii = 0;
+		while (ii < n)
 		{
 			_putchar('#');
+			ii++;
 		}
+
 		_putchar('\n');
+		i++;
 	}
+	if (i == 0)
+		_putchar('\n');
+
 }
