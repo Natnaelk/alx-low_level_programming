dd7fc0e6b102bf6cfe8e380a835766fa22072fa1
root <root@NATI.localdomain>
2022-03-17 04:47:11 +0300

    n;

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 /**
- * natural_no - Entry Point
- * @n : multiple pf 5 and 3
- * Return:void
+ * main - Entry Point
+ * Return:0
  */
 int main(void)
 {
 	int n = 1024, i, sum;
-	
+
 	sum = 0;
-	
 	for (i = 0; i < n; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;		
-		} 
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
