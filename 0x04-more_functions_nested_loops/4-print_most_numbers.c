7e67face4c87751974fe17ffd0c1c2364c0e2568
root <root@NATI.localdomain>
2022-03-17 17:23:39 +0300

    n

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry Point
+ * Return:numbers
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{	
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
