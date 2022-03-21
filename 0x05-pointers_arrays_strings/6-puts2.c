ecc0b6413e07388cf75001af242d7a50e34ef79b
root <root@NATI.localdomain>
2022-03-22 01:25:31 +0300

    n

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,15 @@
  */
 void puts2(char *str)
 {
-	long i = 0;
-
-	while (str[i] != '\0')
+int i = 0;
+while (str[i] != '\0')
+{
+	if (i % 2 == 0)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
-	_putchar('\n');
+
+	i++;
+}
+_putchar('\n');
 }
