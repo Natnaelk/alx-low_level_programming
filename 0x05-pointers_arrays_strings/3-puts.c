0b67435bb2b8d0bd2d62cda7f4a266d5254ec0a6
root <root@NATI.localdomain>
2022-03-22 00:16:48 +0300

    n;

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#inclulde "main.h"
+/**
+ * _puts - Entry Point
+ * @str : pointer to a string
+ * Return:void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	{
+	_putchar('\n');
+}
