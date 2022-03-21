d090e2484def510e798e16d5b0c6e2c71f4c64c0
root <root@NATI.localdomain>
2022-03-21 23:15:37 +0300

    n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 = Entry Point
- * @*n : character that points to the main function
+ * @n : character that points to the main function
  * Return:void
  */
 void reset_to_98(int *n)
