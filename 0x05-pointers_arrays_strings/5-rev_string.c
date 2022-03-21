7107e5c611ff4f0a30bab8d313b8b29b88b5aaac
root <root@NATI.localdomain>
2022-03-22 00:59:26 +0300

    n

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - Entry Point
+ * @s : the reversed string
+ * Retrun:void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i])
+	i++;
+	while (i--)
+	{
+		s[i] == s[j];
+		j++;
+	}
+
+}
