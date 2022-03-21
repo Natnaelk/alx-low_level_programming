725caab12b04e8a2fa2ac453986aef1ab8992078
root <root@NATI.localdomain>
2022-03-22 01:50:58 +0300

    n

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,24 +6,15 @@
  */
 void puts_half(char *str)
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	while (str[i] != '\0')
-	{
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
 	i++;
-	}
-	if (i % 2 == 0)
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		j = i-1/2;	
+		_putchar(str[i]);
 	}
-	else
-	{
-		j = i/2;
-	}
-	while(str[j] != '\0')
-	{
-		_putchar(str[j]);
-		j++;
-	}
+	_putchar('\n');
 }
