7c4d935f7763c09e986f0f4f0715bae31dce7350
root <root@NATI.localdomain>
2022-03-22 18:52:37 +0300

    n

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,31 +8,22 @@
  */
 int _atoi(char *s)
 {
-	unsigned int result = 0;
-	int sign = 1;
-	int i = 0;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	for (; s[i] != '\0'; ++i)
-	{
-		if (s[i] == '-' && (s[i+1] > 47 && s[i+1] < 58))
-		{
-			if (isspace(s[i+1]) == 0)
-			sign = -1;
-			
-		}
-		if (s[i] > 47 && s[i] < 58)
-		{
-		result = result * 10 + (s[i] - '0');
-		if (s[i+1] < 47 || s[i+1] > 58)
-		{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
 			break;
-		}
-		}
-	}
-	return (sign * result);
+
+	} while (*s++);
+
+	return (num * sign);
 }
