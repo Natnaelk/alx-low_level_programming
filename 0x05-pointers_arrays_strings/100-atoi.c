be34114111639ac39a9cc226b8221c86a044e672
root <root@NATI.localdomain>
2022-03-22 15:46:12 +0300

    n

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _atoi - A function that converts aschii ot integer
+ * @s : the string to be converted to integer
+ * Return:integer 
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 0;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	for (; str[i] != '\0'; ++i)
+	{
+		result = result * 10 + (s[i] - '0');
+	}
+	return (sign * result);
+}
