e02a31c698b584e0adff2a78dc6b102ac1642b2f
root <root@NATI.localdomain>
2022-03-24 00:04:17 +0300

    n

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,34 +7,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int lengthd = 0;
-	int lengths = 0;
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	char tmp[10];
+	char *p = dest;
 
-	while (dest[i] != '\0')
-	{
-		lengthd++;
-		i++;
-	}
-	while (src[k] != '\0')
-	{
-		lengths++;
-		k++;
-		tmp[lengths] = src[k];
-	}
-	printf("%d\n", lengthd);
-	printf("%d\n", lengths);
-	for (; j < lengths-1; j++)
-	{
-		dest[lengthd + 1] = tmp[j];
-	}
-	dest[lengthd + 1] = '\0';
-	printf("%d\n", lengthd);
-	printf("%d\n", lengths);
-	printf("%s\n", dest);
-	printf("%s\n", src);
+	while ( *p ) ++p;
+	while ( ( *p++ = *src++ ) != '\0' );
 	return (dest);
+
 }
