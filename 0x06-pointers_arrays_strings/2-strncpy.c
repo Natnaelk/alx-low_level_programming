6565e12b50bd5ad01cb169921b4923cd534e9264
root <root@NATI.localdomain>
2022-03-24 10:31:17 +0300

    n

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (src[i] != '\0' && i < n)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
-	dest[i] = '\0';
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
