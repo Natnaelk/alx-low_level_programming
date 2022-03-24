ec05b61289ec9d87d337810ff4e2d159dd9111b4
root <root@NATI.localdomain>
2022-03-24 17:25:14 +0300

    n

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - a function that changes a string to upper case
+ * @a : string array
+ * Return:char
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
