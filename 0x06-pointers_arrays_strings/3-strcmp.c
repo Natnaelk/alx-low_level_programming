f05aaed539b45bceb2dcffff084ee9e2e96719b3
root <root@NATI.localdomain>
2022-03-24 15:05:21 +0300

    n

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares two strings
+ * @s1 : first character
+ * @s2 : second character
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i]!= '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return s1[i] - s2[i];
+			break;
+		}
+		i++;
+	}
+	return 0;
+}
