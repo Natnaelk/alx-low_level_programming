68f58a6857d6db451638021f87730a02da4cebc4
root <root@NATI.localdomain>
2022-03-28 22:30:02 +0300

    m

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - Entry Point
+ * @dest : destination character
+ * @src : source pointer
+ * n: integer size
+ * Return:pointer destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
