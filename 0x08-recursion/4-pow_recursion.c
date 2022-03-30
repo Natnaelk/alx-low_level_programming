4022908caa4ab73dfc0ae3fa18be8fd6d7d836fa
root <root@NATI.localdomain>
2022-03-30 20:03:17 +0300

    n

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - Entry Point
+ * @x : the number to be powered
+ * @y : the superscript
+ * Return:int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (-1);
+	return (x * _pow_recursion(y - 1));
+}
