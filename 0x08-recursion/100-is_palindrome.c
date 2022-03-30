200843ac58c30bcc48ead158f1bab751eec395ae
root <root@NATI.localdomain>
2022-03-31 01:15:48 +0300

    ng

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,31 +1,51 @@
 #include "main.h"
-int length(char *a)
+/**
+ * last_index - returns the last index of a string (counts the null char)
+ * @s: pointer the string
+ * Return: int
+ */
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int pair);
+int last_index(char *s)
 {
-	int len = 0;
+int n = 0;
 
-	if (*a != '\0')
-	{
-		len += length(a + 1) + 1;
-	}
-	return (len);
+if (*s > '\0')
+	n += last_index(s + 1) + 1;
+
+return (n);
 }
-int palindrome(char *s,int start, int end, int middle)
-{
-	if ((s[start] == s[end] && middle != 0) || (s[start] == s[end + 1] && middle == 0))
-	{
-		return (1);
-	}
-	else if (s[start] != s[end])
-	{
-		return (0);
-	}
-	else
-		return palindrome(s, start + 1, end - 1, middle);
-}
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
 int is_palindrome(char *s)
 {
-	int len;
+int end = last_index(s);
 
-	len = length(s);
-	return palindrome(s, 0, len - 1, len % 2);
+return (check(s, 0, end - 1, end % 2));
+}
+
+/**
+ * check - checker for the palindrome
+ * @s: string
+ * @start: int moves from right to left
+ * @end: int moves from left to right
+ * @pair: int
+ * Return: 0 or 1
+ */
+
+
+int check(char *s, int start, int end, int pair)
+{
+
+if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+	return (1);
+else if (s[start] != s[end])
+	return (0);
+else
+	return (check(s, start + 1, end - 1, pair));
 }
