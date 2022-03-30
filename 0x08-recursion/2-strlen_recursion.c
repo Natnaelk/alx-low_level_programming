82e9b7b7014e04889d3a9e38d3d66a7d7eec8cb0
root <root@NATI.localdomain>
2022-03-30 17:11:56 +0300

    n

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Entry Point
+ * @s : character
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s == '\0')
+		return;
+	length++;
+	_strlen_recursion(s++);
+	return length;
+}
