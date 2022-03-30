4e3d65da3c6e581a3824e8449df5868f8f47a98d
root <root@NATI.localdomain>
2022-03-30 17:37:45 +0300

    n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - Entry Point
+ * @n: number
+ * Return:int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return n * factorial(n - 1);
+}
