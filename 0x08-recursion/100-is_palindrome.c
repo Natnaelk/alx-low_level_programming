d79cb60350ff727448b93ffa9d51c139b7cf116d
root <root@NATI.localdomain>
2022-03-31 01:07:07 +0300

    ng

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+int length(char *a)
+{
+	int len = 0;
+
+	if (*a != '\0')
+	{
+		len += length(a + 1) + 1;
+	}
+	return (len);
+}
+int palindrome(*s,int start, int end, int middle)
+{
+	if ((s[start] == s[end] && middle != 0) || (s[start] == s[end + 1] && middle == 0))
+	{
+		return (1);
+	}
+	else if (s[start] != s[end])
+	{
+		return (0);
+	}
+	else
+		return palindrome(*s, start + 1, end - 1, middle)
+}
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = length(s);
+	return palindrome(*s, 0, length - 1, length % 2);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,4 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
