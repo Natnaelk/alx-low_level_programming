60d81504acbd0add4b1c7c5ad6e05c9e1f755203
root <root@NATI.localdomain>
2022-04-01 13:31:01 +0300

    n

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc : argument counter
+ * @argv : string
+ * Return:integer
+ */
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	if (argc == 1)
+		printf("%d\n", 0); 
+	else
+		printf("%d\n", argc - 1);
+	return (0);
+}
