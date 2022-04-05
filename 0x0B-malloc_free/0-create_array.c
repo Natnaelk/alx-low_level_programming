c7623e7fbf5c7175c60a6fbda9539c5d43386609
root <root@NATI.localdomain>
2022-04-05 21:05:58 +0300

    n

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - Entry Point
+ * @size : integer size
+ * @c : character c
+ * Return:character
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *d;
+
+	d = malloc(size);
+	d[0] = c;
+	return (d);
+}
