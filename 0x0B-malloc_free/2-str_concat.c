45b0b47a2be739b706022a6a4ae93d966780bbdc
root <root@NATI.localdomain>
2022-04-06 00:14:41 +0300

    jej

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int leng = 0;
 	char *s;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
 	while (s1[i] != '\0')
 	{
 		leng++;
