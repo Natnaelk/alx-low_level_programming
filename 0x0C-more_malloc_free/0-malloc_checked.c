9318a8e501b88e7a401d4b6b8ead9501938e8283
root <root@NATI.localdomain>
2022-04-07 21:00:55 +0300

    n

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc : Entry Point
+ * @b : integer no
+ * Return:void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *s = malloc(b);
+	if (s == 0)
+	{
+		exit(98);
+	}
+	return (s);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat - a function that concatenates two strings
+ * @s1 : string one
+ * @s2 : string two
+ * n : size of s2 to be printed
+ * Return:character
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0;
+	int j;
+	int k;
+	int length;
+	char *p;
+
+	j = 0;
+	k = 0;
+	while (s1[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	p = malloc(length + n + 1);
+
+	if (p == 0)
+		return (NULL);
+	while (s1[j] != '\0')
+	{
+		p[j] = s1[j];
+		j++;
+	}
+	while (n > 0)
+	{
+		p[j] = s2[k];
+		n--;
+		k++;
+		j++;
+	}
+	p[j + 1] = '\0';
+	return (p);
+}
