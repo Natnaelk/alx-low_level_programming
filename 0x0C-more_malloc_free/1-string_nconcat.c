d3a0dd7508d82ced55ef0d25b1a76c6c0a49a1ed
root <root@NATI.localdomain>
2022-04-07 22:38:30 +0300

    n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,41 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * _strlen - Length of string
+ * @s : string
+ * Return:int
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+/**
  * string_nconcat - a function that concatenates two strings
  * @s1 : string one
  * @s2 : string two
- * n : size of s2 to be printed
+ * @n : size of s2 to be printed
  * Return:character
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int m = 0;
-	unsigned int j;
-	unsigned int k;
-	unsigned int length1 = 0;
-	unsigned int length2 = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
 	char *p;
 
-	j = 0;
-	k = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i] != '\0')
+	if (n < _strlen(s2))
 	{
-		length1++;
-		i++;
-	}
-	while (s2[m] != '\0')
-	{
-		length2++;
-		m++;
-	}
-	if (n < length2)
-	{
-		p = malloc(length1 + n + 1);
+		p = malloc(_strlen(s1) + n + 1);
 	}
 	else
-		p = malloc(length1 + length2 + 1);
+		p = malloc(_strlen(s1) + _strlen(s2) + 1);
 
 	if (p == 0)
 		return (NULL);
