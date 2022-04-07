78918cbba0113030d57c4a46fb7e00b991731a24
root <root@NATI.localdomain>
2022-04-07 22:07:23 +0300

    n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	j = 0;
 	k = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (s1 == NULL && s2 == NULL)
+	{
+		*s1 = '\0';
+		*s2 = '\0';
+	}
 	while (s1[i] != '\0')
 	{
 		length++;
