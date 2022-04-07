ab5a51ae26f05b0f0915185b9cb02d15066760a6
root <root@NATI.localdomain>
2022-04-07 21:33:16 +0300

    n

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
+
 /**
- * _realloc : Entry Point
- * @b : integer no
- * Return:void
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
  */
+
 void *malloc_checked(unsigned int b)
 {
-	int *s = malloc(b);
+int *m = malloc(b);
+if (m == 0)
+	exit(98);
 
-	if (s == 0)
-		exit(98);
-	return (s);
+return (m);
 }
