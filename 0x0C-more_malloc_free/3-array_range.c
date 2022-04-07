1b3db64f05b8a4f864d2cfda6f53ed5e0713a38c
root <root@NATI.localdomain>
2022-04-07 23:42:42 +0300

    m

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - Entry Point
+ * @min : minimum range
+ * @max : maximum range
+ * Return:int
+ */
+int *array_range(int min, int max)
+{
+	int i = max - min;
+	int k = min;
+	int j = 0;
+	int *p = malloc((i + 1) * sizeof(int));
+
+	if (min > max)
+		return (NULL);
+	if (p == 0)
+		return (NULL);
+	while (j < i)
+	{
+		p[j] = k;
+		j++;
+		k++;
+	}
+	return (p);	
+}
