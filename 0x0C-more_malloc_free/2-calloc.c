bd2371528e1c3ac3fa546f5608d1d04805cfa9d7
root <root@NATI.localdomain>
2022-04-07 23:05:45 +0300

    ng

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *ptr = s;
+
+while (n--)
+	*s++ = b;
+
+return (ptr);
+}
+
+/**
  * _calloc - a function that acts as calloc built in function
  * @nmemb : no of elements or members
  * @size : size of the array
@@ -9,14 +27,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int length = nmemb * size;
 	unsigned int i = 0;
-	char *p = malloc(length);
+	void *p = malloc(length);
 
 	if (p == 0 || nmemb == 0 || size == 0)
 		return (NULL);
-	while (i < length)
-	{
-		p[i] = 0;
-		i++;
-	}
+	_memset(m, 0, length);
 	return (p);
 }
