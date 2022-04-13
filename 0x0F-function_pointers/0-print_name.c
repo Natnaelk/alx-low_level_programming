cae2c51221ce9f89abedec8b15958abcf6d0575a
root <root@NATI.localdomain>
2022-04-13 23:42:34 +0300

    n

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 /**
  * print_name - A function that prints a name
  * @name : char name
- * @f : pointer f
- * Return:void
+ * @f : pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
