4fb3895b75198137fde89a578dd57cf23927cd46
root <root@NATI.localdomain>
2022-04-14 00:00:48 +0300

    b

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,16 @@
 #include "function_pointers.h"
 /**
  * @array_iterator - function
+ * @array : integer
  * @size : size of the array
  * @action : pointer
+ * Return:void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	long unsigned int i = 0;
 
-	while (action != NULL && i < size)
+	while (array != NULL && action != NULL && i < size)
 	{
 		action(array[i]);
 		i++;
