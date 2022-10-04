6f76d7601358a99fc7dfd4e80ea98440dca2564b
Leantan <natnaelk46@gmail.com>
2022-10-04 17:55:10 +0300

    linter fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,20 +2,21 @@
 
 /**
  * linear_search - Entry point
- * @*array: array to be searched
+ * @array: array to be searched
  * @size: size of the array
  * @value: the value of the array index
  * Return:int
  */
 int linear_search(int *array, unsigned int size, int value)
 {
-	unsigned int i = 0;
-	for(; i < size; i++)
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
 	{
-		if ( array[i] == value )
+		if (array[i] == value)
 		{
 			return (i);
-		}		
+		}
 	}
 	return (-1);
 }
